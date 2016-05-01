0b124adf464002d74cafa19e35b4f54713a12e1e
sameer <sameer.ranade@ymail.com>
2016-04-30 20:36:59 -0700

    updated patient details

--- public/javascripts/controllers/patientController.js
+++ public/javascripts/controllers/patientController.js
@@ -16,6 +16,19 @@ angular.module('ehrApp', []).controller('patientController', function ($scope, $
         var url = 'http://localhost:3000/patients/' + pName + '/';
         $http.get(url).success(function (res) {
             console.log(res);
+
+            $scope.pname = pName;
+            $scope.uid = res[0].uer_id;
+            $scope.height = res[0].hight;
+            $scope.weight = res[0].weight;
+            $scope.address = res[0].address;
+            $scope.phone = res[0].phone;
+            $scope.emerphone = res[0].emergency_contact;
+            $scope.bgroup = res[0].blood_type;
+            $scope.gender = res[0].gender;
+            $scope.ethnicity = res[0].ethinicity;
+            $scope.languages = res[0].languagetype;
+            $scope.bday = res[0].Birthday;
         });
     }
 });
