060c42e0a7237fc6d601fa5bcd82462311d77ae9
sameer <sameer.ranade@ymail.com>
2016-05-06 16:46:38 -0700

    updated landing page layout and CSS

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ var router = express.Router();
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'nishanth',
-    database: 'yelp',
+    password: 'SRmay@123',
+    database: 'EHR',
     port : '3306'
 });
 
