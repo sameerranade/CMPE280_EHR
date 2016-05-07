a9559a3d32f1697be4b3b2fb6effee4b730f5b0b
sameer <sameer.ranade@ymail.com>
2016-05-07 16:32:22 -0700

    updated patient details view with charts

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ var router = express.Router();
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'nishanth',
-    database: 'yelp',
+    password: 'SRmay@123',
+    database: 'EHR',
     port : '3306'
 });
 
@@ -101,6 +101,10 @@ router.get('/patients/:pName', function (req, res, data) {
         }
 
     });
+});
+
+router.get('/patientSummary', function (req, res, next) {
+    res.render('patientSummary');
 });
 
 module.exports = router;
