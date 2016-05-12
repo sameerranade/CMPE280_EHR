b3ef96981c2d17098b335bd6b339799e6d05bd27
nishanth-prabhuswamy <nishanth.prabhu.68@gmail.com>
2016-05-12 10:59:42 -0700

    SEO www redirect

--- app.js
+++ app.js
@@ -57,7 +57,13 @@ if (app.get('env') === 'development') {
     });
   });
 }
-
+app.get('/*', function(req, res, next) {
+  if (req.headers.host.match(/^www/) !== null ) {
+    res.redirect('http://' + req.headers.host.replace(/^www\./, '') + req.url);
+  } else {
+    next();
+  }
+})
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
